32bb46a78784b704daeebefc9f911ef9c5e2d3fb
Peter Bourgon <peter@bourgon.org>
2014-09-01 19:49:51 +0200

    Turn 'hello, world' into a web server

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import "net/http"
+
 func main() {
-	println("hello!")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello!"))
 }
